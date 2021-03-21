689311b4351c1bb2b864dd9a521cca5c16b2b6ea
JohnB17 <63297273+JohnB17@users.noreply.github.com>
2021-03-21 17:54:11 -0400

    Add files via upload

--- src/main/java/com/johnb/johnsfirstplugin/CommandKit.java
+++ src/main/java/com/johnb/johnsfirstplugin/CommandKit.java
@@ -1,5 +1,6 @@
 package com.johnb.johnsfirstplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,20 +11,27 @@ import org.bukkit.inventory.ItemStack;
 public class CommandKit implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
+        if (args[0] == "test") {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                // Create a new ItemStack (type: diamond)
+                ItemStack diamond = new ItemStack(Material.DIAMOND);
+
+                // Create a new ItemStack (type: brick)
+                ItemStack bricks = new ItemStack(Material.BRICK);
+
+                // Set the amount of the ItemStack
+                bricks.setAmount(20);
+
+                // Give the player our items (comma-separated list of all ItemStack)
+                player.getInventory().addItem(bricks, diamond);
+            }
+        } else {
             Player player = (Player) sender;
-
-            // Create a new ItemStack (type: diamond)
-            ItemStack diamond = new ItemStack(Material.DIAMOND);
-
-            // Create a new ItemStack (type: brick)
-            ItemStack bricks = new ItemStack(Material.BRICK);
-
-            // Set the amount of the ItemStack
-            bricks.setAmount(20);
-
-            // Give the player our items (comma-separated list of all ItemStack)
-            player.getInventory().addItem(bricks, diamond);
+            player.sendMessage(ChatColor.RED + "Sorry! That kit doesn't exist.");
+            player.sendMessage(ChatColor.GREEN + "These are the existing kits:");
+            player.sendMessage(ChatColor.GRAY + "-" + ChatColor.GREEN + "Test");
         }
 
         // If the player (or console) uses our command correct, we can return true
